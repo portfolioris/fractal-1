55f1e5954691faedbb761150f137bf3aa0df9381
Joris Hulsbosch <joris@portfolioris.nl>
2020-06-27 16:10:03 +0200

    add some form elements

---
+++ src/patterns/00-atoms/00-forms/00-label/label.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  context: {
+    isLegend: false,
+    for: 'input-id',
+    label: 'label',
+    hint: null,
+  },
+};
---
+++ src/patterns/00-atoms/00-forms/01-inputtext/inputtext.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  context: {
+    modifier: '',
+    id: 'input-text',
+    name: 'input-text',
+    type: 'text',
+    placeholder: '',
+    value: false,
+    isRequired: true,
+    isReadonly: false,
+    autocomplete: null,
+    data: 'data-module-bind="validate-input"',
+  },
+};
