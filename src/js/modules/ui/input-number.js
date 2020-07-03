7163a7aea942be66e3706b8f247df9909df92d37
Joris Hulsbosch <joris@portfolioris.nl>
2020-07-03 08:51:48 +0200

    update status of input-number

--- src/js/modules/ui/input-number.js
+++ src/js/modules/ui/input-number.js
@@ -4,6 +4,7 @@ class InputNumber {
     this.$addBtn = this.$el.querySelector('[data-module-bind*=input-number-add]');
     this.$removeBtn = this.$el.querySelector('[data-module-bind*=input-number-remove]');
     this.$input = this.$el.querySelector('[data-module-bind*=input-number-input]');
+    this.$status = this.$el.querySelector('[data-module-bind*=input-number-status]');
     this.step = parseFloat(this.$input.step);
     this.min = parseFloat(this.$input.min);
     this.max = parseFloat(this.$input.max);
@@ -26,12 +27,14 @@ class InputNumber {
     // if value is not a number, comparison fails silently
     if (this.getInputValue() + this.step <= this.max) {
       this.$input.value = this.getInputValue() + this.step;
+      this.$status.innerHTML = this.$input.value;
     }
   }
 
   decrease() {
     if (this.getInputValue() - this.step >= this.min) {
       this.$input.value = this.getInputValue() - this.step;
+      this.$status.innerHTML = this.$input.value;
     }
   }
 }
