e4377386556d031a907f0d9ecceb9619927a0dfb
Joris Hulsbosch <joris@portfolioris.nl>
2020-09-16 11:53:02 +0200

    use logical props, refactor select to grid

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,9 @@
 module.exports = {
   plugins: {
     autoprefixer: {},
-    'postcss-normalize': {},
     'postcss-pxtorem': {
       propList: ['*'],
       minPixelValue: 4,
     },
-    // 'postcss-logical': {
-    //   preserve: true,
-    // },
   },
 };
