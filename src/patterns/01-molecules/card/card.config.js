619a45e26fe1bb68f17c23692f5993201a6b022d
Joris Hulsbosch <joris@portfolioris.nl>
2020-11-02 14:58:23 +0100

    setup card in mesh example

--- src/patterns/01-molecules/card/card.config.js
+++ src/patterns/01-molecules/card/card.config.js
@@ -8,5 +8,30 @@ module.exports = {
       element: 'span',
       label: 'Read more',
     },
+    image: {
+      width: 200,
+      height: 100,
+      isLazy: true,
+      alt: 'a placeholder',
+      src: '//placehold.it/200x100',
+      srcset: [
+        {
+          src: '//placehold.it/200x100',
+          width: 200,
+        },
+        {
+          src: '//placehold.it/400x200',
+          width: 400,
+        },
+        {
+          src: '//placehold.it/600x300',
+          width: 600,
+        },
+        {
+          src: '//placehold.it/800x400',
+          width: 800,
+        },
+      ],
+    },
   },
 };
