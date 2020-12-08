b687c6b3f0eda46f3c2a74723cb45ff5da005edf
Joris Hulsbosch <joris.hulsbosch@weareyou.com>
2020-12-08 14:15:07 +0100

    refactor accordeon animation to waapi

--- src/js/modules/ui/accordeon.js
+++ src/js/modules/ui/accordeon.js
@@ -3,22 +3,19 @@ import { $ } from '../../utilities';
 class Accordeon {
   constructor($el) {
     this.$el = $el;
-    this.$summary = $($el, '[data-module-bind=accordeon-summary]');
-    this.$container = $(this.$el, '[data-module-bind=accordeon-container]');
-    this.$content = $(this.$el, '[data-module-bind=accordeon-content]');
-
-    this.removeTransitionOpen = this.removeTransitionOpen.bind(this);
-    this.removeTransitionClose = this.removeTransitionClose.bind(this);
+    this.$summary = $(this.$el, '[data-module-bind=accordeon-summary]');
+    this.$content = $(this.$el, '[data-module-bind=accordeon-container]');
+    this.animation = null;
+    this.isClosing = false;
+    this.isOpening = false;
+    this.transitionDuration =
+      parseFloat(window.getComputedStyle(this.$content).transitionDuration) * 1000;
   }
 
   init() {
-    this.addEventListeners();
-    this.$el.classList.add('is-initialized');
-  }
-
-  addEventListeners() {
     this.$summary.addEventListener('click', (e) => {
       e.preventDefault();
+      this.$el.style.overflow = 'hidden';
 
       if (!this.$el.open) {
         this.open();
@@ -29,35 +26,68 @@ class Accordeon {
   }
 
   open() {
-    this.$el.setAttribute('open', 'open');
-    this.$el.classList.add('is-open');
-    const dims = this.$content.getBoundingClientRect();
-    this.$container.addEventListener('transitionend', this.removeTransitionOpen);
-    this.$container.style.height = `${dims.height}px`;
+    // Apply a fixed height on the element
+    this.$el.style.height = `${this.$el.offsetHeight}px`;
+    this.$el.open = true;
+    window.requestAnimationFrame(() => this.expand());
   }
 
-  removeTransitionOpen() {
-    this.$container.style.height = 'auto';
-    this.$container.removeEventListener('transitionend', this.removeTransitionOpen);
+  expand() {
+    this.isExpanding = true;
+    // Get the current fixed height of the element
+    const startHeight = `${this.$el.offsetHeight}px`;
+    // Calculate the open height of the element (summary height + content height)
+    const endHeight = `${this.$summary.offsetHeight + this.$content.offsetHeight}px`;
+
+    // If there is already an animation running, cancel
+    if (this.animation) {
+      this.animation.cancel();
+    }
+
+    // Start a WAAPI animation
+    this.animation = this.$el.animate(
+      { height: [startHeight, endHeight] },
+      { duration: this.transitionDuration }
+    );
+    // When the animation is complete, call onAnimationFinish()
+    this.animation.onfinish = () => this.onAnimationFinish(true);
+    // If the animation is cancelled, isExpanding variable is set to false
+    this.animation.oncancel = () => {
+      this.isExpanding = false;
+    };
+  }
+
+  onAnimationFinish(open) {
+    this.$el.open = open;
+    this.animation = null;
+    this.isClosing = false;
+    this.isExpanding = false;
+    this.$el.style.height = '';
+    this.$el.style.overflow = '';
   }
 
   close() {
-    const dims = this.$content.getBoundingClientRect();
-    this.$container.style.height = `${dims.height}px`;
-    this.$el.classList.remove('is-open');
+    this.isClosing = true;
+    // Store the current height of the element
+    const startHeight = `${this.$el.offsetHeight}px`;
+    // Calculate the height of the summary
+    const endHeight = `${this.$summary.offsetHeight}px`;
+    // If there is already an animation running, cancel it
+    if (this.animation) {
+      this.animation.cancel();
+    }
 
-    window.requestAnimationFrame(() => {
-      this.$container.addEventListener('transitionend', this.removeTransitionClose);
+    this.animation = this.$el.animate(
+      { height: [startHeight, endHeight] },
+      { duration: this.transitionDuration }
+    );
 
-      window.requestAnimationFrame(() => {
-        this.$container.style.height = '0';
-      });
-    });
-  }
-
-  removeTransitionClose() {
-    this.$container.removeEventListener('transitionend', this.removeTransitionClose);
-    this.$el.removeAttribute('open');
+    // When the animation is complete, call onAnimationFinish()
+    this.animation.onfinish = () => this.onAnimationFinish(false);
+    // If the animation is cancelled, isClosing variable is set to false
+    this.animation.oncancel = () => {
+      this.isClosing = false;
+    };
   }
 }
 
