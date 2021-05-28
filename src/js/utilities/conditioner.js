e621bddb5a1572f5349a8bf3cd8d485b2e59ec62
Joris Hulsbosch <joris.hulsbosch@weareyou.com>
2021-05-28 12:41:57 +0200

    import conditioner statically

--- src/js/utilities/conditioner.js
+++ src/js/utilities/conditioner.js
@@ -2,44 +2,44 @@
  * Import conditioner
  * As per: https://pqina.nl/conditioner/
  */
-import('conditioner-core/conditioner-core.esm').then(conditioner => {
-  conditioner.addPlugin({
-    // converts module aliases to paths
-    moduleSetName: (name) => `${name}`,
-    // moduleSetName: (name) => `/../${ name }.js`,
-    // get the module constructor
-    moduleGetConstructor: (module) => module.default,
-    // override the import
-    moduleImport: (name) => import(`../modules/${name}.js`),
-  });
+import * as conditioner from 'conditioner-core/conditioner-core.esm';
 
-  conditioner.addPlugin({
-    // the plugin "monitor" hook
-    monitor: {
-      // the name of our monitor, not prefixed with "@"
-      name: 'visible',
-
-      // the monitor factory method, this will create our monitor
-      create: (context, element) => ({
-        // current match state
-        matches: false,
-
-        // called by conditioner to start listening for changes
-        addListener(change) {
-          new IntersectionObserver((entries) => {
-            // update the matches state
-            this.matches = entries.pop().isIntersecting === context;
-
-            // inform conditioner of the new state
-            change();
-          }).observe(element);
-        },
-      }),
-    },
-  });
-
-  /**
-   * Fire up the modules!
-   */
-  conditioner.hydrate(document.documentElement);
+conditioner.addPlugin({
+  // converts module aliases to paths
+  moduleSetName: (name) => `${name}`,
+  // moduleSetName: (name) => `/../${ name }.js`,
+  // get the module constructor
+  moduleGetConstructor: (module) => module.default,
+  // override the import
+  moduleImport: (name) => import(`../modules/${name}.js`),
 });
+
+conditioner.addPlugin({
+  // the plugin "monitor" hook
+  monitor: {
+    // the name of our monitor, not prefixed with "@"
+    name: 'visible',
+
+    // the monitor factory method, this will create our monitor
+    create: (context, element) => ({
+      // current match state
+      matches: false,
+
+      // called by conditioner to start listening for changes
+      addListener(change) {
+        new IntersectionObserver((entries) => {
+          // update the matches state
+          this.matches = entries.pop().isIntersecting === context;
+
+          // inform conditioner of the new state
+          change();
+        }).observe(element);
+      },
+    }),
+  },
+});
+
+/**
+ * Fire up the modules!
+ */
+conditioner.hydrate(document.documentElement);
